a4d72a1aed450372ae21420981074551b2b30f8f
EC2 Default User <ec2-user@ip-172-31-30-50.ap-northeast-1.compute.internal>
2023-10-29 10:00:55 +0000

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-   root to:"public_homes#top"
+      # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+  }
+  root to:"public_homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20231028213035_create_items.rb
@@ -0,0 +1,22 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      ## Database authenticatable
+      t.string :email, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+      t.string :first_name, null: false
+      
+      t.string :name, null: false #null: false 空白は間違い
+      t.text :introduction, null: false
+      t.integer :price, null: false
+      t.timestamps
+    end
+  end
+end
